6cdb881ac388be4ce8570ffeb443c5cd832827dd
MGregos <ev_12_93@mail.ru>
2020-04-07 14:18:27 +0300

    task_7 (fixed again)

--- taks_7/src/by/hardynets/task_7/Houses.java
+++ taks_7/src/by/hardynets/task_7/Houses.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Houses {
 
     public static void main(String[] args) {
+        int temp = 0;
 
 
 // Мы народ не особо богатый, поэтому ограничимся участком в 100 соток
@@ -12,73 +13,84 @@ public class Houses {
         System.out.println("Введите число сторону участка e (макс 100):");
         int e = in.nextInt();
 
-        if (e < 0 || e > 100) {
-            System.out.println("Вы, барин, сильно размахнулис. Уходим");
+        if (e <= 0 || e > 100) {
+            System.out.println("Сторона участка 'e' слишком большая, или введено неверное значение. Уходим.");
             System.exit(0);
 
         } else System.out.println("Введите сторону участка f (макс 100):");
 
         int f = in.nextInt();
-        if (f < 0 || f > 100) {
-            System.out.println("Вы, барин, сильно размахнулис. Уходим");
+        if (f <= 0 || f > 100) {
+            System.out.println("Сторона участка 'f' слишком большая, или введено неверное значение. Уходим.");
             System.exit(0);
+            }
+            if (f > e) {
+                temp = e;
+                e = f;
+                f = temp;
         }
 
         System.out.println("Введите сторону 1  дома 1 (макс 100):");
 
         int a = in.nextInt();
-        if (a < 0 || a > 100) {
+        if (a <= 0 || a > 100) {
             System.out.println("Сторона 1 дома 1 слишком большая, или введено неверное значение. Уходим.");
             System.exit(0);
         } else System.out.println("Введите сторону 2  дома 1 (макс 100):");
 
 
         int b = in.nextInt();
-        if (b < 0 || b > 100) {
+        if (b <= 0 || b > 100) {
             System.out.println("Сторона 2 дома 1 слишком большая, или введено неверное значение. Уходим.");
             System.exit(0);
         } else System.out.println("Проверяем вместительность дома №1 на участок");
 
+        if (b > a) {
+            temp = a;
+            a = b;
+            b = temp;
+        }
 
-        if ((a > e || b > f) || (a > f || b > e)) {
+        if (a > e || b > f) {
             System.out.println("Увы, дом №1 на участок не поместится. Уходим.");
             System.exit(0);
         } else System.out.println("Дом №1 поместился");
 
         // Теперь нам нужно убрать из участка наш дом. Создадим 2 новых прямоугольника
 
-        int e_1 = e - a;
-        int f_1 = f - b;
 
-        if (e_1 == 0 && f_1 == 0) {
-            System.out.print("Но занял весь участок. Уходим.");
-            System.exit(0);
-        }
+            int e_1 = e - a;
+            int f_1 = f - b;
+
+            if (e_1 == 0 && f_1 == 0) {
+                System.out.print("Но занял весь участок. Уходим.");
+                System.exit(0);
+            }
 
 
             System.out.println("Введите сторону 1  дома 2 (макс 100):");
 
-        int c = in.nextInt();
-        if (c < 0 || c > 100) {
-            System.out.println("Сторона 1 дома 1 слишком большая, или введено неверное значение. Уходим.");
-            System.exit(0);
-        } else System.out.println("Введите сторону 2  дома 2 (макс 100):");
+            int c = in.nextInt();
+            if (c <= 0 || c > 100) {
+                System.out.println("Сторона 1 дома 2 слишком большая, или введено неверное значение. Уходим.");
+                System.exit(0);
+            } else System.out.println("Введите сторону 2  дома 2 (макс 100):");
 
 
-        int d = in.nextInt();
-        if (d < 0 || d > 100) {
-            System.out.println("Сторона 2 дома 2 слишком большая, или введено неверное значение. Уходим.");
-            System.exit(0);
-        } else System.out.println("Проверяем вместительность дома 2 на участок\n");
+            int d = in.nextInt();
+            if (d <= 0 || d > 100) {
+                System.out.println("Сторона 2 дома 2 слишком большая, или введено неверное значение. Уходим.");
+                System.exit(0);
+            } else System.out.println("Проверяем вместительность дома 2 на участок\n");
 
 
+            if ((c <= e_1 && d <= f) || (d <= e_1 && c <= f) || (c <= f_1 && d <= e) || (d <= f_1 && c <= e)) {
+                System.out.println("Дом №2 помещается на участке вместе с домом №1");
+                System.exit(0);
+            } else System.out.println("Места на участке для дома заданного зазмера не осталось");
 
 
-        if ((c <= e_1 && d <= f) || (d <= e_1 && c <= f) || (c <= f_1 && d <= e) || (d <= f_1 && c <= e)) {
-            System.out.println("Дом №2 помещается на участке вместе с домом №1");
-            System.exit(0);
-        } else System.out.println("Места на участке для дома заданного зазмера не осталось");
-
-
+        }
     }
-}
+
+
