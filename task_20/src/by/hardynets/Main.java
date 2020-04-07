3d5e2134a7e38ef0632c61043a8537dfe80e7468
MGregos <ev_12_93@mail.ru>
2020-04-07 13:00:36 +0300

    task_20 (fixed)

--- task_20/src/by/hardynets/Main.java
+++ task_20/src/by/hardynets/Main.java
@@ -1,9 +1,20 @@
 package by.hardynets;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        String str = "    Выходы хореографу хомченко легализовать     ";
+
+        // Для быстроты проверки алгоритма ввод с консоли закомментировал.
+
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите строку:");
+        String str = scanner.nextLine();
+         */
+
+
+        String str = "     Test! Fesf? Lesl, Pesp - Nesn:      (Guesg) Resr.      ";
         str = ((str.trim()));
         char symbol;
         char symbol2;
@@ -12,11 +23,23 @@ public class Main {
         for (int i = 0; i < t - 1; i++) {
             symbol = str.charAt(i);
             symbol2 = str.charAt(i + 1);
-            if (i == 0 || (symbol != ' ' && symbol2 == ' ')) {
+            if (i == 0 || (symbol != ' ' && symbol2 == ' ')
+                    || symbol2 == '!' || symbol2 == '?' || symbol2 == '.' || symbol2 == ','
+                    || symbol2 == '(' || symbol2 == ')' || symbol2 == '-' || symbol2 == ':' || symbol2 == ';') {
+                if (symbol == '!' || symbol == '?' || symbol == '.' || symbol == ','
+                        || symbol == '(' || symbol == ')' || symbol == '-'
+                        || symbol == ':' || symbol == ';' || symbol == ' ') {
+                    continue;
+                }
                 System.out.print(symbol);
 
             }
-            if (i == (t - 2) || symbol == ' ' && symbol2 != ' ') {
+            if (i == (t - 2) || (symbol == ' ' && symbol2 != ' ') || symbol == '(') {
+                if (symbol2 == '!' || symbol2 == '?' || symbol2 == '.' || symbol2 == ','
+                        || symbol2 == '(' || symbol2 == ')' || symbol2 == '-'
+                        || symbol2 == ':' || symbol2 == ';') {
+                    continue;
+                }
                 System.out.print(symbol2);
             }
         }
